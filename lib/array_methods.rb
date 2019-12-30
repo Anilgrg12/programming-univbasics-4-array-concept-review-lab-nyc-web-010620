0ce8612cbb316ea5fc6ca81767302d45236c6153
amusing-transpiler-2573 <anil_grg@hotmail.com>
2019-12-30 23:28:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,9 +8,12 @@ end
 end
 
 def find_max_value(array)
+  counter = 0
   length=array.length
-  while length<1 do 
-    puts array.max
+  while length< 1 do 
+    puts [array].max
+    counter += 1 
+  end 
 end
 
 def find_min_value(array)
